d2e5a3702a4a6dcd1f003689912b89b040ff7429
issac_wong <stw09701125@gmail.com>
2018-09-30 20:40:53 +0800

    test file modified

--- test.cpp
+++ test.cpp
@@ -47,37 +47,37 @@ bigbed::HeaderType test1_header_ans
     bigbed::BBIHeader {0x8789f2eb, 4, 2, 1142, 2046, 4179, 12, 12, 304, 1038, 16384, 1078}
   , bigbed::ChromList 
     {
-	{"chr1", 0, 249250621, std::vector<bigbed::Offset>()}
-      , {"chr10", 1, 135534747, std::vector<bigbed::Offset>()}
-      ,	{"chr11", 2, 135006516, std::vector<bigbed::Offset>()}
-      ,	{"chr12", 3, 133851895, std::vector<bigbed::Offset>()}
-      ,	{"chr13", 4, 115169878, std::vector<bigbed::Offset>()}
-      , {"chr14", 5, 107349540, std::vector<bigbed::Offset>()}
-      , {"chr15", 6, 102531392, std::vector<bigbed::Offset>()}
-      , {"chr16", 7, 90354753, std::vector<bigbed::Offset>()}
-      , {"chr17", 8, 81195210, std::vector<bigbed::Offset>()}
-      , {"chr17_ctg5_hap1", 9, 1680828, std::vector<bigbed::Offset>()}
-      ,	{"chr18", 10, 78077248, std::vector<bigbed::Offset>()}
-      , {"chr19", 11, 59128983, std::vector<bigbed::Offset>()}
-      , {"chr1_gl000192_random", 12, 547496, std::vector<bigbed::Offset>()}
-      , {"chr2", 13, 243199373, std::vector<bigbed::Offset>()}
-      , {"chr20", 14, 63025520, std::vector<bigbed::Offset>()}
-      , {"chr21", 15, 48129895, std::vector<bigbed::Offset>()}
-      , {"chr22", 16, 51304566, std::vector<bigbed::Offset>()}
-      ,	{"chr3", 17, 198022430, std::vector<bigbed::Offset>()}
-      , {"chr4", 18, 191154276, std::vector<bigbed::Offset>()}
-      , {"chr5", 19, 180915260, std::vector<bigbed::Offset>()}
-      , {"chr6", 20, 171115067, std::vector<bigbed::Offset>()}
-      , {"chr7", 21, 159138663, std::vector<bigbed::Offset>()}
-      , {"chr8", 22, 146364022, std::vector<bigbed::Offset>()}
-      , {"chr9", 23, 141213431, std::vector<bigbed::Offset>()}
-      , {"chrM", 24, 16571, std::vector<bigbed::Offset>()}
-      , {"chrUn_gl000220", 25, 161802, std::vector<bigbed::Offset>()}
-      , {"chrUn_gl000223", 26, 180455, std::vector<bigbed::Offset>()}
-      , {"chrUn_gl000228", 27, 129120, std::vector<bigbed::Offset>()}
-      , {"chrUn_gl000229", 28, 19913, std::vector<bigbed::Offset>()}
-      , {"chrX", 29, 155270560, std::vector<bigbed::Offset>()}
-      , {"chrY", 30, 59373566, std::vector<bigbed::Offset>()}
+	{"chr1", 0, 249250621, {{2054, 87}}}
+      , {"chr10", 1, 135534747, {{2141, 74}}}
+      ,	{"chr11", 2, 135006516, {{2215, 78}}}
+      ,	{"chr12", 3, 133851895, {{2293, 74}}}
+      ,	{"chr13", 4, 115169878, {{2367, 75}}}
+      , {"chr14", 5, 107349540, {{2442, 49}}}
+      , {"chr15", 6, 102531392, {{2491, 47}}}
+      , {"chr16", 7, 90354753, {{2538, 78}}}
+      , {"chr17", 8, 81195210, {{2616, 45}}}
+      , {"chr17_ctg5_hap1", 9, 1680828, {{2661, 44}}}
+      ,	{"chr18", 10, 78077248, {{2705, 46}}}
+      , {"chr19", 11, 59128983, {{2751, 45}}}
+      , {"chr1_gl000192_random", 12, 547496, {{2796, 45}}}
+      , {"chr2", 13, 243199373, {{2841, 75}}}
+      , {"chr20", 14, 63025520, {{2916, 47}}}
+      , {"chr21", 15, 48129895, {{2963, 90}}}
+      , {"chr22", 16, 51304566, {{3053, 92}}}
+      ,	{"chr3", 17, 198022430, {{3145, 90}}}
+      , {"chr4", 18, 191154276, {{3235, 93}}}
+      , {"chr5", 19, 180915260, {{3328, 88}}}
+      , {"chr6", 20, 171115067, {{3416, 73}}}
+      , {"chr7", 21, 159138663, {{3489, 85}}}
+      , {"chr8", 22, 146364022, {{3574, 73}}}
+      , {"chr9", 23, 141213431, {{3647, 88}}}
+      , {"chrM", 24, 16571, {{3735, 54}}}
+      , {"chrUn_gl000220", 25, 161802, {{3789, 76}}}
+      , {"chrUn_gl000223", 26, 180455, {{3865, 44}}}
+      , {"chrUn_gl000228", 27, 129120, {{3909, 46}}}
+      , {"chrUn_gl000229", 28, 19913, {{3955, 57}}}
+      , {"chrX", 29, 155270560, {{4012, 78}}}
+      , {"chrY", 30, 59373566, {{4090, 89}}}
     }
 };
 
@@ -100,13 +100,36 @@ bigbed::BBMemberType default_bb_ans
 
 bigbed::BBMemberType test1_bb_ans
 {
-
+    "chr1"
+  , 1815107
+  , 1815204
+  , "LSU-rRNA_Hsa\t0\t+\t1815107\t1815204\t0\t1\t97\t0"
 };
 
 bigbed::BBMemberType test2_bb_ans
 {
-
+    "chr1"
+  , 4417098
+  , 4417211
+  , "LSU-rRNA_Hsa\t0\t-\t4417098\t4417211\t0\t1\t113\t0"
 };
+
+bigbed::BBMemberType test3_bb_ans
+{
+    "chr10"
+  , 200793
+  , 200880
+  , "5S\t0\t-\t200793\t200880\t0\t1\t87\t0"
+};
+
+bigbed::BBMemberType test4_bb_ans
+{
+    "chr10"
+  , 327975
+  , 328065
+  , "5S\t0\t-\t327975\t328065\t0\t1\t90\t0"
+};
+
 
 // TEST start
 
@@ -207,17 +230,24 @@ TEST(BigBed, member_function)
     std::ifstream ifile("../test.bb", std::ios::binary);
     Header testh1(ifile);
     BigBed test1(testh1);
-    //BigBed test2(testh1);
-    //BigBed test3(testh1);
-    //BigBed test4(testh1);
-    BigBed::get_obj(ifile, test1);
     BigBed test2(testh1);
-    BigBed::get_obj(ifile, test2);
     BigBed test3(testh1);
-    BigBed::get_obj(ifile, test3);
     BigBed test4(testh1);
+    BigBed::get_obj(ifile, test1);
+    //BigBed test2(testh1);
+    BigBed::get_obj(ifile, test2);
+    //BigBed test3(testh1);
+    BigBed::get_obj(ifile, test3);
+    //BigBed test4(testh1);
     BigBed::get_obj(ifile, test4);
-    
+    static_loop_check_EQ<0>(test1.get_header(), test1_header_ans);
+    static_loop_check_EQ<0>(test1, test1_bb_ans);
+    static_loop_check_EQ<0>(test2.get_header(), test1_header_ans);
+    static_loop_check_EQ<0>(test2, test2_bb_ans);
+    static_loop_check_EQ<0>(test3.get_header(), test1_header_ans);
+    static_loop_check_EQ<0>(test3, test3_bb_ans);
+    static_loop_check_EQ<0>(test4.get_header(), test1_header_ans);
+    static_loop_check_EQ<0>(test4, test4_bb_ans);
 }
 
 /*
